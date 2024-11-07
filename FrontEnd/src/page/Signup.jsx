5e4fa970d48207ab4b0a7e414bdd0a12887e9a7a
aharshsingh <aharshsingh25@gmail.com>
2024-11-07 13:03:40 +0530

    Signup page frontend

--- FrontEnd/src/page/Signup.jsx
+++ FrontEnd/src/page/Signup.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import '../componentCSS/Signup.css';  
 import { Navigate, Link } from 'react-router-dom';
-
+import taskIcon from '../images/images__1_-removebg-preview.png';
+import gradbg from '../images/Screenshot 2024-11-05 154810.png';
 const SignUp = () => {
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
@@ -33,25 +34,35 @@ const SignUp = () => {
   }
 
   return (
+    <div style={{display:'flex', columnGap:'0px'}}>
     <div className="signup-container">
-      <h2 >Sign Up</h2>
+      <div className='heading'>
+        <img className='TaskIcon' src={taskIcon} alt='icon' />
+        <p style={{fontSize:'15px',marginTop:'10px'}}>Task Manager</p>
+      </div>
+      <div className='formStyle'>
+      <h2 style={{fontSize:'35px', fontWeight:'100', color:'rgb(56,56,56)', textAlign:'left', marginTop:'40px'}}>Sign Up</h2>
       <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label >Username</label>
-          <input  type="text" value={userName} onChange={(e) => setUserName(e.target.value)} required />
+        <div className="form-group" >
+          <p style={{fontSize:'15px',marginTop:'20px', textAlign:'left', opacity:'0.65'}}>Create an account and Start today. Every task completed brings you one step closer to your goals.</p>
+          <label style={{marginTop:'25px'}}>Username</label>
+          <input  type="text" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder='Your username' required />
         </div>
-        <div className="form-group">
+        <div className="form-group" >
           <label >Email</label>
-          <input  type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <input  type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='abc@gmail.com' required />
         </div>
         <div className="form-group">
           <label >Password</label>
-          <input  type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <input  type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='************' required />
         </div>
         <button type="submit">Sign Up</button>
-        <Link to='/Signin' className='login'><p style={{fontSize:'15px', marginTop:'20px'}}>Already Signed Up? Login</p></Link>
+        <Link to='/Signin' className='login'><p className='alreadyLogin' style={{fontSize:'15px', marginTop:'20px'}}>Already Signed Up? Login</p></Link>
       </form>
+      </div>
       {message && <p>{message}</p>}
+    </div>
+    <img src={gradbg} alt='iamge' style={{height:'900px', width:'600px', margin: '150px auto', marginLeft:'0px'}}/>
     </div>
   );
 };
