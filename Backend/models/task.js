b93d18f1253e0263aaafdf95c57bda7dba4c058f
aharshsingh <aharshsingh25@gmail.com>
2025-02-21 13:28:50 +0530

    gmail smtp

--- Backend/models/task.js
+++ Backend/models/task.js
@@ -5,8 +5,6 @@ const Schema = mongoose.Schema;
 const taskSchema = new Schema({
     taskName: { type: String, required: true },
     description: { type: String, required: true },
-    deadline: { type: Date, required: true },
-    priority: {type: String, required: true},
     userId: {type: String, required: true},
     iscompleted: {type: Boolean, required: true, default: false}
 }, { timestamps: true});
